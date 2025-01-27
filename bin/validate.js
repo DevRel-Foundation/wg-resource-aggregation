1b361ccec2112783a9cbb9789957f9334b9a9272
j12y <jayson.delancey@gmail.com>
2025-01-26 22:04:37 -0800

    feat(validate): check a batch of assets and report validation summary

--- bin/validate.js
+++ bin/validate.js
@@ -1,25 +1,54 @@
-// node validate.js --schema ../events/events.schema.json --data ../events/data/fossdem.org.json
+// Given a schema validate any given data files against the schema
+// Validate a single file:
+//   node validate.js --schema ../events/events.schema.json --data ../events/data/fossdem.org.json
+// Validate an entire collection:
+//   node validate.js --schema ../events/events.schema.json --data ../events/data/*.json
 
 const fs = require('fs');
 const args = require('yargs')
-    .command('--schema', 'schema to use for validation')
-    .command('--data', 'data files to validate')
+    .command('schema', 'schema to use for validation')
+    .option('data', {type: 'array', desc: 'data files to validate'})
+    .option('verbose', {type: 'boolean'})
     .demand(['data','schema'])
     .argv;
 
 const Ajv = require("ajv/dist/2020"); // https://ajv.js.org/guide/schema-language.html#draft-2019-09-and-draft-2020-12
 const ajv = new Ajv({ strict: false })
 
+// Load schema
 const schema = JSON.parse(fs.readFileSync(args.schema, 'utf8'));
 const validate = ajv.compile(schema);
 
-const data = JSON.parse(fs.readFileSync(args.data, 'utf8'));
-const valid = validate(data);
-if (valid) {
-    console.log("Looks Good");
-} else {
-    console.log("Invalid Resource");
-    console.log(validate.errors);
+let report = {
+    valid: 0,
+    invalid: 0,
+    review: []
 }
 
+// Iterate over each data file to validate it
+args.data.forEach(item => {
+    log(item); 
+    const asset = JSON.parse(fs.readFileSync(item, 'utf8'));
+    if (validate(asset)) {
+        log("Valid");
+        report.valid++;
+    } else {
+        log("Invalid");
+        report.invalid++;
+        report.review.push(item);
+        log(validate.errors);
+        console.warn(`${item} => ${JSON.stringify(validate.errors)}`);
+    }
+});
 
+if (report.invalid > 0) {
+    console.error(`Failed to validate:\n${report.review.join('\n')}`)
+}
+console.log(`Validated ${report.valid} out of ${args.data.length} resources checked.`)
+
+
+function log(msg) {
+    if (args.verbose) {
+        console.log(msg)
+    }
+}
